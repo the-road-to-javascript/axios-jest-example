10f1e5489f53d6ecf8ca64de29e02bf23e099170
Robin Wieruch <wrobin@gmx.net>
2019-11-19 12:40:38 +0800

    first commit

---
+++ src/test.js
@@ -0,0 +1,42 @@
+import axios from 'axios';
+
+import { fetchData, API } from './';
+
+jest.mock('axios');
+
+describe('fetchData', () => {
+  it('fetches successfully data from an API', async () => {
+    const data = {
+      data: {
+        hits: [
+          {
+            objectID: '1',
+            title: 'a',
+          },
+          {
+            objectID: '2',
+            title: 'b',
+          },
+        ],
+      },
+    };
+
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
+
+    const url = `${API}/search?query=react`;
+
+    await expect(fetchData('react')).resolves.toEqual(data);
+
+    expect(axios.get).toHaveBeenCalledWith(url);
+  });
+
+  it('fetches erroneously data from an API', async () => {
+    const errorMessage = 'Network Error';
+
+    axios.get.mockImplementationOnce(() =>
+      Promise.reject(new Error(errorMessage)),
+    );
+
+    await expect(fetchData('react')).rejects.toThrow(errorMessage);
+  });
+});
